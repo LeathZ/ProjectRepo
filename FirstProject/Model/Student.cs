2f44d5085c82b280bafbd837ed21755a2d2b5c5b
Lajesha Sambahamphe <123lajesha@gmail.com>
2024-11-25 07:53:54 +0545

    first commit

---
+++ FirstProject/Model/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstProject.Model;
+
+public class Student
+{
+    public int ID { get; set; }
+    public int FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+    public string Address { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedBy { get; set; }
+}
