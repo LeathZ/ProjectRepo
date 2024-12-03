e9a411855fcd1f663d7a7a2ac18bb28d51d85257
Lajesha Sambahamphe <123lajesha@gmail.com>
2024-12-03 08:14:17 +0545

    Second Commit

---
+++ FirstProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using FirstProject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace RepositoryPattern_ASPNetCore_WebAPI.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }public DbSet<Student> Students { get; set; }
+}
